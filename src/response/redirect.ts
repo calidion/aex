81b428158d27b6fbfe52be515e681396bcacd53b
calidion <calidion@gmail.com>
2021-01-29 10:50:06 +0800

    enabled redirect

--- src/response/redirect.ts
+++ src/response/redirect.ts
@@ -3,7 +3,7 @@ import { ServerResponse } from "http";
 export function redirect(res: ServerResponse) {
   Object.defineProperty(res, "redirect", {
     enumerable: false,
-    value (url: string) {
+    value(url: string) {
       res.writeHead(301, { Location: url });
       res.end();
     },
