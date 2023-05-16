10768e5c2a99aa366f945ff288ba21df333f3603
calidion <calidion@gmail.com>
2023-05-16 14:02:39 +0800

    init customizable server

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,30 @@
+import assert = require("assert");
 import { createServer, RequestListener } from "http";
-import { IServer } from "./types";
+import { IServer, IServerCreator } from "./types";
 
 export async function start(
   serveringCallback: RequestListener,
   port: number,
   ip: string
 ): Promise<IServer> {
-  return new Promise((resolve, reject) => {
-    const server = createServer(serveringCallback);
+  const creator: IServerCreator = {
+    createServer(options: any, cb: RequestListener): IServer {
+      assert(options === null);
+      return createServer(cb);
+    },
+  };
+  return customStart(creator, null, serveringCallback, port, ip);
+}
 
+export async function customStart(
+  creator: IServerCreator,
+  options: any,
+  serveringCallback: RequestListener,
+  port: number,
+  ip: string
+): Promise<IServer> {
+  return new Promise((resolve, reject) => {
+    const server = creator.createServer(options, serveringCallback);
     server.listen(port, ip);
     server.on("error", (error: Error) => {
       reject(error);
